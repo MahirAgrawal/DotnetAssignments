ef9c1446cf41d7bdb10b945741bee46270867e84
= <mihiragrawal022@gmail.com>
2022-01-25 16:54:04 +0530

    First and Second Assignment done

---
+++ Datatypes&Conditional&Loop/Datatypes/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("Integer: \n");
+          snippet0();
+          snippet1();
+          snippet2();
+          snippet3();
+          snippet4();
+          snippet5();
+          Console.WriteLine("\nDouble: \n");
+          snippet6();
+          snippet7();
+          snippet8();
+          Console.WriteLine("\nDecimal: \n");
+          snippet9();
+          snippet10();
+          snippet11();
+          snippet12();
+        }
+        static void snippet0(){
+          int a = 1, b = 10,c = a+b;
+          Console.WriteLine("Add: " + c);
+          c = a-b;
+          Console.WriteLine("Subtract: " + c);
+          c = a*b;
+          Console.WriteLine("Multiply: " + c);
+          c = a/b;
+          Console.WriteLine("Divide: " + c);
+        }
+        static void snippet1(){
+        //showing BODMAS
+          int a = 1, b = 10,c = 4,d = a+b*c;
+          Console.WriteLine($"Snippet1: {d}");
+        }
+        static void snippet2(){
+          int a = 1, b = 10,c = 4,d = (a+b)*c;
+          Console.WriteLine($"Snippet2: {d}");
+          d = (a + b) / c;
+          Console.WriteLine($"Snippet2: {d}");
+        }
+        static void snippet3(){
+          int a = 1, b = 10,c = 4,d = (a+b)/c,e = (a+b)%3;
+          Console.WriteLine($"The Quotient is: {d}");
+          Console.WriteLine($"The Remainder is: {e}");
+        }
+        static void snippet4(){
+          Console.WriteLine($"The minValue for int is: {int.MinValue} & the maxValue for int is: {int.MaxValue}");
+        }
+        static void snippet5(){
+          int max = int.MaxValue;
+          int overflowbydumbmachine = max + 3;
+          Console.WriteLine($"The Value for int is: {overflowbydumbmachine}");
+        }
+        static void snippet6(){
+          double a = 1, b = 10,c = 4,d = (a+b)/c;
+          Console.WriteLine($"Double ans: {d}");
+        }
+        static void snippet7(){
+          Console.WriteLine($"The minValue for double is: {double.MinValue} & the maxValue for double is: {double.MaxValue}");
+        }
+        static void snippet8(){
+          double third = 1.0 / 3.0;
+          Console.WriteLine(third);
+        }
+        static void snippet9(){
+          Console.WriteLine($"The minValue for Decimal is: {decimal.MinValue} & the maxValue for Decimal is: {decimal.MaxValue}");
+        }
+        static void snippet10(){
+          Console.WriteLine("Double : " + 1.0 / 3.0);
+          Console.WriteLine("Decimal : " + 1.0M / 3.0M);
+        }
+        static void snippet11(){
+          //calculate the area of circle by using PI in math
+          double radius = 2.5;
+          Console.WriteLine($"The Area of Circle is {Math.PI*radius*radius} cm2\n");
+        }
+        static void snippet12(){
+        }
+    }
+}
---
+++ Datatypes&Conditional&Loop/Loopandcondition/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	  Console.WriteLine("\nConditional Statements: \n");
+          snippet0();
+          snippet1();
+          snippet2();
+	  Console.WriteLine("\nLoop Statements: \n");
+          snippet3();
+          snippet4();
+          snippet5();
+          snippet6();
+          snippet7();
+        }
+        static void snippet0(){
+          int a = 5;
+          int b = 1;
+          if (a + b > 10)
+            Console.WriteLine("The answer is greater than 10.");
+        }
+        static void snippet1(){
+          int a = 5;
+          int b = 1;
+          if (a + b > 10)
+            Console.WriteLine("The answer is greater than 10.");
+          else
+            Console.WriteLine("The answer is smaller than or equal to 10");          
+        }
+        static void snippet2(){
+          int a = 5,b = 10 ,c = 10;
+          if((a+b+c > 10) || (b == c)){
+            Console.WriteLine("The summation is larger than 10 or b equals c");
+          }
+          else{
+            Console.WriteLine("The summation is smaller than 10 and b not equals c");
+          }
+        }
+        static void snippet3(){
+          int counter = 0;
+          while(counter < 10){
+            Console.WriteLine($"While loop I am! The Counter is {counter}");
+            counter++;
+          }
+        }
+        static void snippet4(){
+          //run and then checks the condition
+          int counter = 0;
+          do{
+            Console.WriteLine($"Do while I am! The Counter is {counter}");
+            counter++;
+          }while(counter < 10);         
+        }
+        static void snippet5(){
+          for(int counter = 0; counter < 10; counter++)
+          {
+            Console.WriteLine($"For loop I am! The Counter value is {counter}");
+          }
+        }
+        static void snippet6(){
+          //nested loops 
+          int row = 5,col = 2;
+          for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+              Console.WriteLine("The row and col: " + i + " " + j);
+            }
+          }
+        }
+        static void snippet7(){
+          //  find the sum of all integers 1 through 20 that are divisible by 3
+          int sum = 0;
+          for(int i = 1; i <= 20; i++){
+            if(i%3 == 0){
+              sum += i;
+            }
+          }
+          Console.WriteLine("The Sum is : " + sum);
+        }
+    }
+}
